3f4901db4f86c2824d19c66e7704f04d7ed5e3b3
AJ ONeal <awesome@coolaj86.com>
2014-12-30 00:13:06 -0700

    demo w/ local.foobar3000.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  "salt": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-, "shadow": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  "salt": "e2c5a1b4-3fdf-49af-87f2-7d0e650d284a"
+, "shadow": "j1mPZKEbw4Rn51juQJ+3QoAggt90Vw47k1Tdw/Cqh+zs+mgzRFNWk1Abfp+VgnHE"
 };
